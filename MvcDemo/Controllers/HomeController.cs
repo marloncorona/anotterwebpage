c79eaa3406b29fbe21b3e5c1a8cf79bff1c34b97
Marlon Corona Arango <marloncoronaarango@Marlons-MacBook-Pro.local>
2025-11-19 18:59:20 -0800

    Implementacion final - Despliegue con Google Cloud

--- MvcDemo/Controllers/HomeController.cs
+++ MvcDemo/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-/*
- * Comentario: Controlador MVC. 
- * Contiene acciones (métodos) que responden a rutas e invocan vistas.
- */
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using MvcDemo.Models;
@@ -10,21 +6,17 @@ namespace MvcDemo.Controllers;
 
 public class HomeController : Controller
 {
-    // Accion GET / o /Home/Index
+    // home/index
     public IActionResult Index()
     {
-        // Comentario: Aquí podrías cargar datos del modelo/BD y pasarlos a la vista
         ViewData["Message"] = "Bienvenido a ASP.NET Core MVC 🚀";
         return View();
     }
-
-    // GET /Home/Agendar
     public IActionResult Agendar()
     {
         return View();
     }
 
-    // GET /Home/Error
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
